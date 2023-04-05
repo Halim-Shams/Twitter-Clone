fa6ccda22874799d4520dd02470d1840fd7fbeba
Halim-Shams <halimshams8@gmail.com>
2023-04-05 15:28:58 +0430

    side_nav added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def is_active?(url_path)
+    if current_page?(url_path)
+      'font-bold'
+    else
+      ''
+    end
+  end
+
+  def is_active_logo?(url_path)
+    if current_page?(url_path)
+      'currentColor'
+    else
+      'none'
+    end
+  end
 end
