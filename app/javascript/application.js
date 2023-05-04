75b97f0c884ee5795dedd613a433680cc3f0600d
Halim-Shams <halimshams8@gmail.com>
2023-05-04 18:19:19 +0430

    Auth added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails';
+import 'controllers';
+
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+
+function autosize() {
+	var el = this;
+	setTimeout(function () {
+		el.style.cssText = 'height:auto; padding:0';
+		// for box-sizing other than "content-box" use:
+		// el.style.cssText = '-moz-box-sizing:content-box';
+		el.style.cssText = 'height:' + el.scrollHeight + 'px';
+	}, 0);
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_093246) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_30_125956) do
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
